eb92ce3df5317a29d4ddb5dfa621e87868ea5f12
Raveesh <newtonlovesheadbanging@gmail.com>
2015-07-04 14:54:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var zetta = require('zetta');
 var LED = require(' zetta-led-edison-driver')
-
-zetta().name('Raveesh-Sharma').use(LED,13).listen(1337,function(){
+var BUZZER = require('zetta-buzzer-edison-driver');
+zetta().name('Raveesh-Sharma').use(LED,13).use(BUZZER,3).listen(1337,function(){
 console.log('Zetta server is running on port 1337');
 });
